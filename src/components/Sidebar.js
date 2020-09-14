1e89748a442dac1841743bcdeac12e74d1f554cc
Geetguud <maman021129@gmail.com>
2020-09-14 21:36:42 +0700

    Add custom sizes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,12 +5,30 @@ function Sidebar(props) {
     const {initialActive, activeLeft, mistakes, initializing} = props;
     const progress = Math.round(((initialActive - activeLeft) / initialActive) * 10000) / 100;
     const [opted, setOpted] = useState("10x10");
+    const [sizeX, setSizeX] = useState("");
+    const [sizeY, setSizeY] = useState("");
 
     function optin(event) { setOpted(event.target.value); }
 
-    function newgame() {
-        const size = opted.split("x").map(each => parseInt(each));
-        initializing(size);
+    function customsize(event) {
+        const { name, value } = event.target;
+        let val = parseInt(value);
+        if (val > 20) { val = 20; }
+        else if (val < 1) { val = 0; }
+        if (name === "x") { setSizeX(val); }
+        else if (name === "y") { setSizeY(val); }
+        else return
+    }
+
+    function newgame(event) {
+        event.preventDefault();
+        if (opted === "custom") {
+            if (!Number.isInteger(sizeX) || !Number.isInteger(sizeY)) { return }
+            initializing([sizeX, sizeY]);
+        } else {
+            const size = opted.split("x").map(each => parseInt(each));
+            initializing(size);
+        }
     }
 
     return (
@@ -19,7 +37,7 @@ function Sidebar(props) {
             <p className="progress">oofs: {mistakes}</p>
             <p className="progress">progress: {progress}%</p>
             <p className="progress">tile left: {activeLeft}</p>
-            <div className="settings flex-center">
+            <form className="settings flex-center" onSubmit={newgame}>
                 <p className="setup">setting</p>
                 <label className="setup">size:
                     <select className="size-opt setting-opt" value={opted} onChange={optin}>
@@ -30,10 +48,22 @@ function Sidebar(props) {
                         <option value="15x15">15 x 15</option>
                         <option value="20x15">20 x 15</option>
                         <option value="20x20">20 x 20</option>
+                        <option value="custom">custom</option>
                     </select>
                 </label>
-                <button className="newgame setting-opt" onClick={() => newgame()}>new game</button>
-            </div>
+                {opted === "custom" ?
+                    <div>
+                        <label className="custom">x:
+                            <input type="number" className="custom-input setting-opt" name="x" value={sizeX} placeholder="horizontal size" onChange={customsize} max="20" min="1" />
+                        </label>
+                        <label className="custom">y:
+                            <input type="number" className="custom-input setting-opt" name="y" value={sizeY} placeholder="vertical size" onChange={customsize} max="20" min="1" />
+                        </label>
+                    </div>
+                    : null
+                }
+                <button className="newgame setting-opt">new game</button>
+            </form>
             <button className="changelight setting-opt" onClick={() => changelight()}>change light</button>
         </div>
     )
