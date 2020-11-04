564196d8f8e8f2687578e04299589c63be77d25a
Geetguud <maman021129@gmail.com>
2020-11-05 06:50:54 +0700

    Experimenting

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-fetch(process.env.PICROSS);
+console.log(process.env.REACT_APP_PICROSS);
